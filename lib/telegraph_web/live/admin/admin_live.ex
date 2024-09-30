aa5fdae32accf4e1e071467a14ec670b1126a6eb
Max Don <max@don.ee>
2024-09-30 20:15:56 +0300

    Fixed promoting/demoting admins

--- lib/telegraph_web/live/admin/admin_live.ex
+++ lib/telegraph_web/live/admin/admin_live.ex
@@ -13,37 +13,6 @@ defmodule TelegraphWeb.Live.AdminLive do
      ), layout: {TelegraphWeb.Layouts, :admin}}
   end
 
-  def handle_event("make_admin", %{"id" => id}, socket) do
-    make_or_demote_admin(id, true, socket)
-  end
-
-  def handle_event("demote_admin", %{"id" => id}, socket) do
-    make_or_demote_admin(id, false, socket)
-  end
-
-  defp make_or_demote_admin(id, is_admin, socket) do
-    user = Accounts.get_user!(id)
-    current_user = socket.assigns.current_user
-
-    if is_admin && user.id == current_user.id do
-      {:noreply, put_flash(socket, :error, "You cannot promote yourself.")}
-    else
-      case Accounts.update_user(user, %{is_admin: is_admin}) do
-        {:ok, _user} ->
-          {:noreply,
-           socket
-           |> put_flash(
-             :info,
-             "User has been #{if(is_admin, do: "promoted to", else: "demoted from")} admin."
-           )
-           |> assign(users: Accounts.list_users())}
-
-        {:error, _changeset} ->
-          {:noreply, put_flash(socket, :error, "Failed to update user.")}
-      end
-    end
-  end
-
   def render(assigns) do
     ~H"""
     <h1><%= @message %></h1>
--- lib/telegraph_web/live/admin/admin_users_live.ex
+++ lib/telegraph_web/live/admin/admin_users_live.ex
@@ -12,6 +12,37 @@ defmodule TelegraphWeb.Live.AdminUsersLive do
      ), layout: {TelegraphWeb.Layouts, :admin}}
   end
 
+  def handle_event("make_admin", %{"id" => id}, socket) do
+    make_or_demote_admin(id, true, socket)
+  end
+
+  def handle_event("demote_admin", %{"id" => id}, socket) do
+    make_or_demote_admin(id, false, socket)
+  end
+
+  defp make_or_demote_admin(id, is_admin, socket) do
+    user = Accounts.get_user!(id)
+    current_user = socket.assigns.current_user
+
+    if is_admin && user.id == current_user.id do
+      {:noreply, put_flash(socket, :error, "You cannot promote yourself.")}
+    else
+      case Accounts.update_user(user, %{is_admin: is_admin}) do
+        {:ok, _user} ->
+          {:noreply,
+           socket
+           |> put_flash(
+             :info,
+             "User has been #{if(is_admin, do: "promoted to", else: "demoted from")} admin."
+           )
+           |> assign(users: Accounts.list_users())}
+
+        {:error, _changeset} ->
+          {:noreply, put_flash(socket, :error, "Failed to update user.")}
+      end
+    end
+  end
+
   def render(assigns) do
     ~H"""
     <h1>Admin Users</h1>
